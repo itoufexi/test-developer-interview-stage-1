2e30f7fd842719cf2181ae999359070a6415ae08
Ioanna Toufexi <itoufexi@gmail.com>
2017-10-29 11:04:56 +0000

    created separate test suites for UI and API

--- src/test/java/APITestSuite.java
+++ src/test/java/APITestSuite.java
@@ -3,6 +3,6 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features="src/test/resources/features/UI/")
-public class UITestSuite {
+@CucumberOptions(features="src/test/resources/features/API/")
+public class APITestSuite {
 }
